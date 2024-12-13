a6e15f1600764361535f340e9d644a60aeb1e223
Danny castrillon <dannycastrillob2@hotmail.com>
2024-12-13 17:43:27 -0500

    Agregar archivos de proyecto.

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using MyLibrary;
+
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageWriter MyWriter = new MessageWriter();
+            MyWriter.write("Hello, writer!");
+
+            FiggleWriter Fwriter = new FiggleWriter();
+            Fwriter.Write("Hello, Figgle!!!");
+        }
+    }
+}
---
+++ MyLibrary/MessageWriter.cs
@@ -0,0 +1,9 @@
+﻿namespace MyLibrary;
+public class MessageWriter
+{
+    public void write(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
